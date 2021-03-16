05300ebe5969e0a21d7eaba4824ad3c087137291
CpedaMonsterEnergyLover <Vova.Cpeda@yandex.ru>
2021-03-16 16:29:49 +0300

    Добавил вторую практику

---
+++ Students/y2335/yagnaev_vladimir/simple_django_web_project/project_first_app/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Owner, Car
+
+
+# creating a form
+class AddOwnerForm(forms.ModelForm):
+    # create meta class
+    class Meta:
+        # specify model to be used
+        model = Owner
+
+        # specify fields to be used
+        fields = [
+            "first_name",
+            "second_name",
+            "birth_date"
+        ]
+
+
+class AddCarForm(forms.ModelForm):
+    # create meta class
+    class Meta:
+        # specify model to be used
+        model = Car
+
+        # specify fields to be used
+        fields = [
+            "brand",
+            "model",
+            "color",
+            "number"
+        ]
